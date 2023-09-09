c959b5fecae74b3088bd8aaac5c2d2bf92e050f0
Shahjehan Ali <shahjehanali1@users.noreply.github.com>
2023-09-09 16:13:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const filterButtons = document.querySelectorAll(".filter-button");
-    const projects = document.querySelectorAll(".project");
+            const filterButtons = document.querySelectorAll(".filter-button");
+            const projects = document.querySelectorAll(".project");
+            const noResultMessage = document.querySelector(".no-result");
 
-    filterButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-            const filterValue = button.getAttribute("data-filter");
-            projects.forEach((project) => {
-                if (filterValue === "all" || project.classList.contains(filterValue)) {
-                    project.style.display = "block";
-                } else {
-                    project.style.display = "none";
-                }
+            filterButtons.forEach((button) => {
+                button.addEventListener("click", () => {
+                    const filterValue = button.getAttribute("data-filter");
+                    let foundProjects = false; // Flag to track if any projects match the filter
+
+                    projects.forEach((project) => {
+                        if (filterValue === "all" || project.classList.contains(filterValue)) {
+                            project.style.display = "block";
+                            foundProjects = true;
+                        } else {
+                            project.style.display = "none";
+                        }
+                    });
+
+                    // Show/hide the "No results" message based on whether any projects matched the filter
+                    if (foundProjects) {
+                        noResultMessage.style.display = "none";
+                    } else {
+                        noResultMessage.style.display = "block";
+                    }
+                });
             });
         });
-    });
-});
